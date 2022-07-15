e1fde7a689f56ac340bf5c15dc6f2c9f5790d89b
VeonardoDaVinci <nikodeminio12@gmail.com>
2022-07-15 23:44:12 +0200

    Add basic files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : Singleton<GameManager>
+{
+
+
+
+    private void Awake()
+    {
+
+    }
+
+}
